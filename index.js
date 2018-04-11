494a01a3f08ab61070351c2b887e088e960b18c4
Bill Kamba <billkamba95@gmail.com>
2018-04-11 06:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(0)
+  return kittens
+}
+
+function appendKitten(name) {
+  var n_arr = [...kittens, name];
+  return n_arr
+}
+
+function prependKitten(name) {
+  var n_arr = [name, ...kittens];
+  return n_arr
+}
+
+function removeLastKitten() {
+  var n_kittens = kittens.slice(0,2)
+  return n_kittens
+}
+
+function removeFirstKitten() {
+  var n_kittens = kittens.slice(1,2)
+  return n_kittens
+}
